cb065399aca1cecf5ea463da90d47ebdedfd0c52
piyush <piyushgk1@gmail.com>
2021-09-07 23:22:07 -0400

    first stab at boilerplate

---
+++ server/model.py
@@ -0,0 +1,24 @@
+from ariadne import QueryType, MutationType
+from uuid import uuid4
+
+class Coffee:
+   def __init__(self, size, name, coffee_type):
+       self.size = size
+       self.name = name
+       self.type = coffee_type
+       self.id = uuid4()
+
+query = QueryType()
+mutation = MutationType()
+
+orders = []
+
+@query.field("orders")
+def resolve_orders(_, info):
+    return orders
+
+@mutation.field("orderCoffee")
+def resolve_order_coffee(_, info, size, name, type):
+    newOrder = Coffee(size, name, type)
+    orders.append(newOrder)
+    return newOrder
---
+++ server/server.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify, send_from_directory
+from ariadne import graphql_sync, make_executable_schema, gql, load_schema_from_path
+from ariadne.constants import PLAYGROUND_HTML
+from model import query, mutation
+import os
+
+type_defs = gql(load_schema_from_path("../schema.graphql"))
+schema = make_executable_schema(type_defs, query, mutation)
+
+
+static_dir = os.path.join('client', 'build')
+
+app = Flask(__name__,
+    root_path=os.path.abspath(".."),
+    static_folder=static_dir,
+    static_url_path='')
+
+
+@app.route('/')
+def root():
+    return send_from_directory(static_dir, "index.html")
+
+@app.route("/graphql", methods=["GET"])
+def graphql_playground():
+    """Serve GraphiQL playground"""
+    return PLAYGROUND_HTML, 200
+
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
