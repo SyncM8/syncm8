a66aa77ea4a5d57aac0f8ef2b31e89d095d72e89
Sammy Baek <kie827@gmail.com>
2021-09-12 18:40:00 -0400

    remove eslint-plugin-react for now

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,10 +5,9 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"],
   },
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:import/recommended",
