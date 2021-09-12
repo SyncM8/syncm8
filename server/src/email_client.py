9a6efd05bfa99f8376506185771f50b7110c7d86
Sammy Baek <kie827@gmail.com>
2021-09-12 19:57:02 -0400

    fix mypy issue

--- server/src/email_client.py
+++ server/src/email_client.py
@@ -1,17 +1,19 @@
 import os
+
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 message = Mail(
-    from_email='admin@syncm8.com',
-    to_emails='kie827@gmail.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    from_email="admin@syncm8.com",
+    to_emails="kie827@gmail.com",
+    subject="Sending with Twilio SendGrid is Fun",
+    html_content="<strong>and easy to do anywhere, even with Python</strong>",
+)
 try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
     response = sg.send(message)
     print(response.status_code)
     print(response.body)
     print(response.headers)
 except Exception as e:
-    print(e.message)
+    print(e)
