4c490b3b4afaeae7dc71966f5664b43833f777af
piyush <piyushgk1@gmail.com>
2021-09-13 19:27:41 -0400

    fix precommit issues

--- server/src/db_client.py
+++ server/src/db_client.py
@@ -4,8 +4,8 @@ from pymongo import MongoClient
 
 uri = os.environ.get("MONGO_URI")
 client = MongoClient(uri)
-db = client['main']
-users = db['users']
+db = client["main"]
+users = db["users"]
 
 cursor = users.find({})
 for document in cursor:
