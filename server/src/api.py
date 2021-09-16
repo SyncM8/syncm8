9c67d30e299ea30131dc980bc1551e5a714da81b
piyush <piyushgk1@gmail.com>
2021-09-15 21:28:17 -0400

    local repo files need dot prepended to their name

--- server/src/api.py
+++ server/src/api.py
@@ -3,7 +3,8 @@ import os
 from ariadne import gql, graphql_sync, load_schema_from_path, make_executable_schema
 from ariadne.constants import PLAYGROUND_HTML
 from flask import Flask, jsonify, request, send_from_directory
-from model import mutation, query
+
+from .model import mutation, query
 
 type_defs = gql(load_schema_from_path("../schema.graphql"))
 schema = make_executable_schema(type_defs, query, mutation)
