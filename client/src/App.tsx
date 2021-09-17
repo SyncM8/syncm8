b6e551ac4d0126a6dc67a821e29734f9a504bc43
Sammy Baek <kie827@gmail.com>
2021-09-17 15:02:38 -0400

    move pageheader out

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,19 @@
 import "./App.less";
 
-import { Button, Col, Input, PageHeader, Row, Tabs, Typography } from "antd";
+import { Button, Typography } from "antd";
 import React, { FC } from "react";
-import { withRouter } from "react-router";
-import { Route, RouteComponentProps, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import DashboardPage from "./components/DashboardPage";
 import FamiliesPage from "./components/FamiliesPages";
 import MatesPage from "./components/MatesPage";
+import PageHeader from "./components/PageHeader";
 
-const { TabPane } = Tabs;
 const { Title } = Typography;
 
-const App: FC<RouteComponentProps> = ({ history }) => (
+const App: FC = () => (
   <>
-    <PageHeader className="site-page-header">
-      <Row>
-        <Col span={8}>
-          <Input.Search placeholder="sync with..." />
-        </Col>
-        <Col offset={8} span={4}>
-          <Tabs
-            onChange={(key) => {
-              history.push(`/${key}`); // eslint-disable-line
-            }}
-            animated={{ inkBar: true, tabPane: false }}
-          >
-            <TabPane tab="Dashboard" key="" />
-            <TabPane tab="Mates" key="mates" />
-            <TabPane tab="Families" key="families" />
-          </Tabs>
-        </Col>
-        <Col span={4}>Profile Here</Col>
-      </Row>
-    </PageHeader>
+    <PageHeader />
     <Switch>
       <Route path="/mates">
         <MatesPage />
@@ -51,4 +31,4 @@ const App: FC<RouteComponentProps> = ({ history }) => (
   </>
 );
 
-export default withRouter(App);
+export default App;
---
+++ client/src/components/PageHeader.tsx
@@ -0,0 +1,59 @@
+import { UserOutlined } from "@ant-design/icons";
+import {
+  Avatar,
+  Col,
+  Dropdown,
+  Input,
+  Menu,
+  PageHeader,
+  Row,
+  Tabs,
+} from "antd";
+import React, { FC } from "react";
+import { withRouter } from "react-router";
+import { Link, RouteComponentProps } from "react-router-dom";
+
+const { TabPane } = Tabs;
+
+const menu = (
+  <Menu>
+    <Menu.Item key="add-mates">
+      <Link to="/add-mates">Add Mates</Link>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="settings">
+      <Link to="/settings">Setting</Link>
+    </Menu.Item>
+  </Menu>
+);
+
+const PageHeaderPage: FC<RouteComponentProps> = ({ history }) => (
+  <>
+    <PageHeader className="site-page-header">
+      <Row>
+        <Col span={8}>
+          <Input.Search placeholder="sync with..." />
+        </Col>
+        <Col offset={8} span={4}>
+          <Tabs
+            onChange={(key) => {
+              history.push(`/${key}`);
+            }}
+            animated={{ inkBar: true, tabPane: false }}
+          >
+            <TabPane tab="Dashboard" key="" />
+            <TabPane tab="Mates" key="mates" />
+            <TabPane tab="Families" key="families" />
+          </Tabs>
+        </Col>
+        <Col span={4}>
+          <Dropdown overlay={menu} trigger={["click"]}>
+            <Avatar size="large" icon={<UserOutlined />} />
+          </Dropdown>
+        </Col>
+      </Row>
+    </PageHeader>
+  </>
+);
+
+export default withRouter(PageHeaderPage);
