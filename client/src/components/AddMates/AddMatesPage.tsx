7452314e773ee2d85b14532e66949917065c4e05
Sammy Baek <kie827@gmail.com>
2021-09-17 20:18:46 -0400

    warn users leaving before saving

--- client/src/components/AddMates/AddMatesPage.tsx
+++ client/src/components/AddMates/AddMatesPage.tsx
@@ -5,25 +5,28 @@ import {
   Divider,
   Form,
   Input,
+  Layout,
   Row,
+  Space,
   Typography,
 } from "antd";
 import React, { FC, useState } from "react";
+import { Prompt } from "react-router";
 
 import { AddMateFormType, AddMateType } from "../types";
 import AddMateCard from "./AddMateCard";
 
 const { Title } = Typography;
+const { Footer } = Layout;
 
 const AddMatesPage: FC = () => {
-  const [form] = Form.useForm();
+  const [form] = Form.useForm<AddMateFormType>();
   const [mates, setMates] = useState<AddMateType[]>([]);
 
-  const onAddMate = ({ name, lastSeen }: AddMateFormType) => {
-    const ts = new Date().getTime();
+  const onAddMate = async ({ name, lastSeen }: AddMateFormType) => {
     setMates((prevArray) => [
       ...prevArray,
-      { name, lastSynced: lastSeen.toDate(), ts },
+      { name, lastSynced: lastSeen.toDate(), ts: new Date().getTime() },
     ]);
     form.resetFields();
   };
@@ -49,45 +52,60 @@ const AddMatesPage: FC = () => {
 
   return (
     <>
-      <Row style={{ backgroundColor: "#F0F2F5", padding: "30px" }}>
-        <Col>
-          <Title>Populating M8s</Title>
-        </Col>
-      </Row>
-      <Row style={{ padding: "30px" }}>
-        <Col span={6}>
-          <Title level={2}>Manual Entry</Title>
-        </Col>
+      <Prompt
+        when={mates.length > 0}
+        message="Leaving will erase all your unsaved info. Are you sure?"
+      />
+      <Layout style={{ minHeight: "100vh" }}>
+        <Row style={{ backgroundColor: "#F0F2F5", padding: "30px" }}>
+          <Col>
+            <Title>Populating M8s</Title>
+          </Col>
+        </Row>
+        <Row style={{ padding: "30px" }}>
+          <Col span={6}>
+            <Title level={2}>Manual Entry</Title>
+          </Col>
 
-        <Col>
-          <Form layout="inline" form={form} onFinish={onAddMate}>
-            <Form.Item label="Name" name="name" rules={[{ required: true }]}>
-              <Input autoFocus />
-            </Form.Item>
-            <Form.Item
-              label="Last Seen"
-              name="lastSeen"
-              rules={[{ required: true }]}
-            >
-              <DatePicker />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Add Mate
-              </Button>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
+          <Col>
+            <Form layout="inline" form={form} onFinish={onAddMate}>
+              <Form.Item label="Name" name="name" rules={[{ required: true }]}>
+                <Input autoFocus />
+              </Form.Item>
+              <Form.Item
+                label="Last Seen"
+                name="lastSeen"
+                rules={[{ required: true }]}
+              >
+                <DatePicker />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Add Mate
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
 
-      <Divider />
+        <Divider />
 
-      <Row style={{ padding: "10px 30px" }}>
-        <Title level={2}>M8s</Title>
-      </Row>
-      <Row style={{ padding: "10px 30px" }} gutter={[24, 24]}>
-        {cards}
-      </Row>
+        <Row style={{ padding: "10px 30px" }}>
+          <Title level={2}>M8s</Title>
+        </Row>
+        <Row style={{ padding: "10px 30px" }} gutter={[24, 24]}>
+          {cards}
+        </Row>
+        <Footer style={{ position: "sticky", bottom: "0" }}>
+          <Space>
+            Once You’ve added all Your M8s (contacts) you can assign them to
+            families
+            <Button type="primary" disabled={mates.length === 0}>
+              Assign M8s to Families
+            </Button>
+          </Space>
+        </Footer>
+      </Layout>
     </>
   );
 };
