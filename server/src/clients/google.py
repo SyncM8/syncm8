6429b2bc9bff25d3f1e86e93f5f3ec086e5fb8cb
piyush <piyushgk1@gmail.com>
2021-09-23 16:36:50 -0400

    test google client

--- server/src/clients/google.py
+++ server/src/clients/google.py
@@ -11,6 +11,7 @@ https://github.com/googleapis/google-api-python-client/blob/main/docs/dyn/index.
 import os
 from typing import Dict, List, Optional, Tuple
 
+import httplib2
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 
@@ -20,7 +21,11 @@ from ..utils.error import AppError, ErrorCode, error_bounded
 @error_bounded(
     (AppError(ErrorCode.GOOGLE_API_ERROR, "Google api error - is token valid"), False)
 )
-def is_google_token_valid(token: str,) -> Tuple[Optional[AppError], bool]:
+def is_google_token_valid(
+    token: str,
+    http: Optional[httplib2.Http] = None,
+    http2: Optional[httplib2.Http] = None,
+) -> Tuple[Optional[AppError], bool]:
     """
     Check wheter or not the JWT is a valid google access token.
 
@@ -29,9 +34,10 @@ def is_google_token_valid(token: str,) -> Tuple[Optional[AppError], bool]:
     - was issued by google
     - is allowed to be used by our app
     """
-    with build("oauth2", "v2") as service:
+    print("called")
+    with build("oauth2", "v2", http=http) as service:
         token_info_request = service.tokeninfo(access_token=token)
-        token_info = token_info_request.execute()
+        token_info = token_info_request.execute(http=http2)
 
         expires_in = token_info.get("expires_in", 0)
         if expires_in <= 0:
@@ -61,7 +67,10 @@ def is_google_token_valid(token: str,) -> Tuple[Optional[AppError], bool]:
     (AppError(ErrorCode.GOOGLE_API_ERROR, "Google api error - get user info"), False)
 )
 def get_user_info(
-    token: str, fields: List[str] = ["name", "email", "picture", "id"]
+    token: str,
+    fields: List[str] = ["name", "email", "picture", "id"],
+    http: Optional[httplib2.Http] = None,
+    http2: Optional[httplib2.Http] = None,
 ) -> Tuple[Optional[AppError], Optional[Dict[str, str]]]:
     """
     Request user's information from google using the provided token.
@@ -69,8 +78,9 @@ def get_user_info(
     Returns the requested fields from the user's information if they exist in
     the form of a dictionary.
     """
-    with build("oauth2", "v2", credentials=Credentials(token)) as service:
-        user_info = service.userinfo().get().execute()
+    creds = Credentials(token) if token else None
+    with build("oauth2", "v2", credentials=creds, http=http) as service:
+        user_info = service.userinfo().get().execute(http=http2)
 
         parsed_user_info = {}
         for field in fields:
