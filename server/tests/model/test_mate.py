af546127941dc9368a06738b65623656cddbf440
Sammy Baek <kie827@gmail.com>
2021-10-31 17:39:02 -0400

    add conftest

--- server/tests/model/test_mate.py
+++ server/tests/model/test_mate.py
@@ -1,10 +1,9 @@
 """Test the mate model."""
 
-from typing import Generator, List
+from typing import List
 from unittest import mock
 
 import pytest
-from mongoengine import connect, disconnect
 from pytest_mock import MockerFixture
 from src.gql.graphql import NewMatesInput
 from src.model.mate import Mate
@@ -14,16 +13,8 @@ mateSteve: NewMatesInput = {"name": "Steve", "lastSynced": "2021-10-23T20:42:06.
 mateJobs: NewMatesInput = {"name": "Jobs", "lastSynced": "2021-09-19T22:11:24.931Z"}
 
 
-@pytest.fixture
-def db_connection() -> Generator[None, None, None]:
-    """Connect to a mock mongodb instance."""
-    disconnect()
-    connect("mongoenginetest", host="mongomock://localhost")
-    yield None
-    disconnect()
-
-
-def test_bulk_insert_new_mates(db_connection: None) -> None:
+@pytest.mark.usefixtures("db_connection")
+def test_bulk_insert_new_mates() -> None:
     """Test bulk inserting mates."""
     new_mates: List[NewMatesInput] = [mateSteve, mateJobs]
     error, mates = Mate.bulk_insert_new_mates(new_mates)
@@ -36,7 +27,8 @@ def test_bulk_insert_new_mates(db_connection: None) -> None:
     assert len(mates[1].sync_ids) == 1
 
 
-def test_bulk_insert_no_last_synced(db_connection: None) -> None:
+@pytest.mark.usefixtures("db_connection")
+def test_bulk_insert_no_last_synced() -> None:
     """Test bulk inserting with no lastSynced."""
     new_mates: List[NewMatesInput] = [{"name": "Steve"}, mateJobs]  # type: ignore
     error, mates = Mate.bulk_insert_new_mates(new_mates)
@@ -46,14 +38,16 @@ def test_bulk_insert_no_last_synced(db_connection: None) -> None:
     assert len(mates[0].sync_ids) == 1
 
 
-def test_bulk_insert_empty(db_connection: None) -> None:
+@pytest.mark.usefixtures("db_connection")
+def test_bulk_insert_empty() -> None:
     """Test inserting empty bulk."""
     error, mates = Mate.bulk_insert_new_mates([])
     assert not error
     assert mates == []
 
 
-def test_bulk_insert_fail_sync(db_connection: None, mocker: MockerFixture) -> None:
+@pytest.mark.usefixtures("db_connection")
+def test_bulk_insert_fail_sync(mocker: MockerFixture) -> None:
     """Test that sync error bubbles up when it fails."""
     new_mates: List[NewMatesInput] = [mateSteve]
     error_msg = "test sync fail"
@@ -68,8 +62,9 @@ def test_bulk_insert_fail_sync(db_connection: None, mocker: MockerFixture) -> No
     assert error.error_details == error_msg
 
 
+@pytest.mark.usefixtures("db_connection")
 @mock.patch("src.model.mate.Mate", Exception())
-def test_bulk_insert_fail(db_connection: None) -> None:
+def test_bulk_insert_fail() -> None:
     """Test mongo error is caught."""
     new_mates: List[NewMatesInput] = [mateSteve]
     error, mates = Mate.bulk_insert_new_mates(new_mates)
