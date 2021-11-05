4e862569173f1c5b1de8785500d7940bf3c97aab
Sam Baek <kie827@gmail.com>
2021-11-04 23:36:53 -0400

    [feat] Maintain url after login (#56)
    ## Changes made
    This PR makes sure that the user's initial URL path is maintained after they log back in. Previously, it would revert them back to the dashboard after logging in.
    
    ## Screencapture (if applicable)
    
    This screencapture shows the following
    - maintain URL while logged in still works (after refresh)
    - maintain URL (`/families`) after being logged out and logging in
    
    https://user-images.githubusercontent.com/12789302/140236840-c542563f-c176-4c76-9096-c32c1d0c56c1.mov
    
    
    ## Work left to be done
    - It would be nice to not flash the login page after logging in? But it's not worth the trouble at the moment.

--- client/src/pages/Login/LoginPage.tsx
+++ client/src/pages/Login/LoginPage.tsx
@@ -1,7 +1,7 @@
 import { Button, notification, Typography } from "antd";
 import axios from "axios";
 import { parse, stringify } from "querystring";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 
 import { loginPath, LoginResponse } from "../../api";
@@ -9,6 +9,8 @@ import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import { LocationState } from "../types";
 
 const { Title } = Typography;
+
+export const PREV_PATH_KEY = "prevPath";
 
 type LoginPageProps = {
   loggedIn: boolean;
@@ -22,13 +24,22 @@ const openNotification = (errorMessage: string) => {
   });
 };
 
-const doLogin = () => {
+/**
+ * Authorize with Google's oAuth
+ * @param prevPath to remember after login
+ */
+const doLogin = (prevPath = "/") => {
+  const state = stringify({
+    [PREV_PATH_KEY]: prevPath,
+  });
+
   const options = {
     client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID ?? "",
     redirect_uri: `${process.env.REACT_APP_HOST_URL ?? ""}/login`,
     response_type: "token",
     include_granted_scopes: "true",
     scope: "openid email profile",
+    state,
   };
 
   window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${stringify(
@@ -36,9 +47,15 @@ const doLogin = () => {
   )}`;
 };
 
+/**
+ * Login Page
+ * @param logingPageProps App's states
+ * @returns JSX.Element
+ */
 const LoginPage = ({ loggedIn, setLoggedIn }: LoginPageProps): JSX.Element => {
   const history = useHistory();
   const location = useLocation<LocationState>();
+  const [beforeLoginPath, setBeforeLoginPath] = useState("/");
 
   const checkloginCallback = () => {
     // window.location.hash keeps the hash, part so we chop it off with substr
@@ -47,6 +64,14 @@ const LoginPage = ({ loggedIn, setLoggedIn }: LoginPageProps): JSX.Element => {
     if ("error" in parsedParams) {
       openNotification(String(parsedParams.error));
     } else if ("access_token" in parsedParams) {
+      const { state } = parsedParams;
+      if (typeof state === "string" && state.includes(PREV_PATH_KEY)) {
+        const newPrevPath = parse(state)[PREV_PATH_KEY];
+        if (typeof newPrevPath === "string") {
+          setBeforeLoginPath(newPrevPath);
+        }
+      }
+
       axios
         .post<LoginResponse>(loginPath, parsedParams)
         .then((res) => {
@@ -60,18 +85,20 @@ const LoginPage = ({ loggedIn, setLoggedIn }: LoginPageProps): JSX.Element => {
     }
   };
 
-  checkloginCallback();
+  useEffect(() => {
+    checkloginCallback();
+  });
 
   // Redirect if authentication is done
   if (loggedIn) {
-    const prevPath = location.state?.prevPath ?? "/";
-    history.replace(prevPath);
+    const replacePath = location.state?.prevPath ?? beforeLoginPath;
+    history.replace(replacePath);
     return <LoadingSpinner />;
   }
   return (
     <>
       <Title>Login</Title>
-      <Button type="primary" onClick={doLogin}>
+      <Button type="primary" onClick={() => doLogin(location.state?.prevPath)}>
         Login with Google
       </Button>
     </>
