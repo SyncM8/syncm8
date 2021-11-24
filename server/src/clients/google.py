0ced7049fc0746cbb94b6c1fc8411b8f7033a42a
achristmascarl <carl.z.liu@gmail.com>
2021-11-23 22:23:49 -0500

    add types to dataclass list)

--- server/src/clients/google.py
+++ server/src/clients/google.py
@@ -9,7 +9,7 @@ https://github.com/googleapis/google-api-python-client/blob/main/docs/dyn/index.
 """
 
 import os
-from typing import Dict, List, Optional, Tuple, Any
+from typing import Any, Dict, List, Optional, Tuple
 
 import httplib2
 from google.oauth2.credentials import Credentials
