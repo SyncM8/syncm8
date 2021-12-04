a69461afa8f4d1b68effdff312471be95415a071
Sammy Baek <kie827@gmail.com>
2021-12-04 10:37:39 -0500

    [feat] Add AssignMates gql frontend
    ghstack-source-id: 659ad86ce51dc4aebebd7ead2eb3fa8cd008fa4d
    Pull Request resolved: https://github.com/SyncM8/syncm8/pull/98

---
+++ client/src/components/UnassignedMateCard/UnassignedMateCard.tsx
@@ -0,0 +1,55 @@
+import "./UnassignedMateCard.css";
+
+import {
+  DeleteOutlined,
+  EllipsisOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card } from "antd";
+import React, { CSSProperties } from "react";
+
+import { Mate } from "../../graphql/types";
+
+type UnassignedMateCardProps = {
+  mate: Mate;
+  lastSynced?: Date;
+  removeMate: (mate: Mate) => void;
+  style?: CSSProperties;
+};
+
+const { Meta } = Card;
+
+const UnassignedMateCard = ({
+  mate,
+  lastSynced,
+  removeMate,
+  style,
+}: UnassignedMateCardProps): JSX.Element => (
+  <Card
+    actions={[
+      <DeleteOutlined key="delete" onClick={() => removeMate(mate)} />,
+      <EllipsisOutlined key="ellipsis" />,
+    ]}
+    style={{
+      ...style,
+      borderRadius: 7,
+      borderBottomRightRadius: 7,
+      borderBottomLeftRadius: 7,
+    }} // CSS magic
+  >
+    <Meta
+      avatar={<Avatar size="small" icon={<UserOutlined />} />}
+      title={mate.name}
+      description={
+        lastSynced && `Last Synced: ${lastSynced.toLocaleDateString()}`
+      }
+    />
+  </Card>
+);
+
+UnassignedMateCard.defaultProps = {
+  style: {},
+  lastSynced: undefined,
+};
+
+export default UnassignedMateCard;
--- client/src/pages/AssignMates/AssignMatesPage.tsx
+++ client/src/pages/AssignMates/AssignMatesPage.tsx
@@ -1,5 +1,15 @@
-import { Button, Col, Divider, Layout, Row, Space, Typography } from "antd";
-import React, { useState } from "react";
+import { useLazyQuery, useMutation } from "@apollo/client";
+import {
+  Button,
+  Col,
+  Divider,
+  Layout,
+  notification,
+  Row,
+  Space,
+  Typography,
+} from "antd";
+import React, { useEffect, useState } from "react";
 import {
   DragDropContext,
   DraggableLocation,
@@ -8,8 +18,9 @@ import {
 import { Prompt } from "react-router";
 
 import FamilyDroppable from "../../components/FamilyDroppable/FamilyDroppable";
-import { NewMateType } from "../types";
-import { initialGroup } from "./mockData";
+import { ASSIGN_MATES, GET_UNASSIGNED_DATA } from "../../graphql/graphql";
+import { Family, Mate, MateAssignmentInput, User } from "../../graphql/types";
+import { UnassignedMate } from "../types";
 
 const { Title } = Typography;
 const { Footer } = Layout;
@@ -58,19 +69,109 @@ const move = <T,>(
 };
 
 /**
+ * Create UnassignedMate from mate's syncs
+ * @param mate
+ * @returns UnassignedMate with lastSynced
+ */
+const populateLastSynced = (mate: Mate): UnassignedMate => {
+  if (mate.syncs?.length > 0) {
+    const date = mate.syncs.reduce(
+      (prev, cur) => (prev > cur ? prev : cur),
+      mate.syncs[0]
+    );
+    return {
+      ...mate,
+      lastSynced: new Date(date.timestamp),
+    };
+  }
+  return { ...mate };
+};
+
+/**
  * AssignMatesPage
  * @returns
  */
 const AssignMatesPage = (): JSX.Element => {
-  const [groups, setGroups] =
-    useState<Record<string, NewMateType[]>>(initialGroup);
+  const [groups, setGroups] = useState<Record<string, UnassignedMate[]>>({});
+  const [familyMap, setFamilyMap] = useState<Record<string, Family>>({});
+  const [unassignedFamilyId, setUnassignedFamilyId] = useState("");
+  const [isAllAssigned, setIsAllAssigned] = useState(false);
+
+  /**
+   * GQL Query to fetch unassigned mates and families info
+   */
+  const [getUnassignedData] = useLazyQuery<{ getUserData: User }>(
+    GET_UNASSIGNED_DATA,
+    {
+      onCompleted: (data) => {
+        const { unassigned_family: unassignedFamily, families } =
+          data.getUserData;
+        const newFamilies = families.reduce(
+          (object, family) => ({
+            ...object,
+            [family.id]: family,
+          }),
+          {}
+        );
+        setFamilyMap(newFamilies);
+        setIsAllAssigned(unassignedFamily.mates.length === 0);
+
+        const initialGroup = {
+          [unassignedFamily.id]: unassignedFamily.mates.map(populateLastSynced),
+        };
+
+        const newGroups = families
+          .filter((family) => family.id !== unassignedFamily.id)
+          .reduce(
+            (object, family) => ({
+              ...object,
+              [family.id]: [],
+            }),
+            initialGroup
+          );
+        setGroups(newGroups);
+        setUnassignedFamilyId(unassignedFamily.id);
+      },
+      onError: (error) => {
+        notification.error({
+          message: error.name,
+          description: error.message,
+        });
+      },
+      fetchPolicy: "network-only",
+    }
+  );
+
+  /**
+   * GQL Mutation for assigning mates
+   */
+  const [assignMatesFn] = useMutation<{ assignMates: string[] }>(ASSIGN_MATES, {
+    onCompleted: (data) => {
+      notification.success({
+        message: `Assigned ${data.assignMates.length} mates!`,
+      });
+      getUnassignedData();
+    },
+    onError: (error) => {
+      notification.error({
+        message: error.name,
+        description: error.message,
+      });
+    },
+  });
+
+  useEffect(() => {
+    getUnassignedData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   /**
    * Removes a mate from this page
    * @param mate
    */
-  const removeMate = (mate: NewMateType): void => {
-    const newGroup: Record<string, NewMateType[]> = Object.keys(groups).reduce(
+  const removeMate = (mate: Mate): void => {
+    // TODO: delete mate from DB
+    const newGroup: Record<string, Mate[]> = Object.keys(groups).reduce(
       (object, family) => ({
         ...object,
         [family]: groups[family].filter((m8) => m8.id !== mate.id),
@@ -80,8 +181,27 @@ const AssignMatesPage = (): JSX.Element => {
     setGroups(newGroup);
   };
 
-  const submitNewAssignments = () => {
-    // TODO send to backend here
+  /**
+   * Submit mate assignments to server
+   */
+  const submitNewAssignments = async () => {
+    const mateAssignments: MateAssignmentInput[] = [];
+    Object.keys(groups)
+      .filter((familyId) => familyId !== unassignedFamilyId)
+      .forEach((familyId) => {
+        groups[familyId].forEach((unassignedMate) => {
+          mateAssignments.push({
+            mateId: unassignedMate.id,
+            fromFamilyId: unassignedFamilyId,
+            toFamilyId: familyId,
+          });
+        });
+      });
+    await assignMatesFn({
+      variables: {
+        mateAssignments,
+      },
+    });
   };
 
   /**
@@ -136,32 +256,34 @@ const AssignMatesPage = (): JSX.Element => {
         </Row>
         <Row style={{ padding: "30px" }}>
           <Col span={6}>
-            <Title level={2}>Unassigned Mates</Title>
+            <Title level={2}>
+              {isAllAssigned ? "All mates assigned!" : "Unassigned Mates"}
+            </Title>
           </Col>
         </Row>
         <FamilyDroppable
-          mates={groups.unassigned}
+          mates={groups[unassignedFamilyId] ?? []}
           direction="horizontal"
-          family="unassigned"
+          droppableId={unassignedFamilyId}
           removeMate={removeMate}
         />
         <Divider />
         <Row wrap={false} style={{ overflow: "auto" }}>
           {Object.keys(groups)
-            .filter((family) => family !== "unassigned")
-            .map((family) => (
+            .filter((familyId) => familyId !== unassignedFamilyId)
+            .map((familyId) => (
               <Col
-                key={family}
+                key={familyId}
                 style={{ width: 220, marginLeft: 5, marginRight: 5 }}
               >
                 <Row justify="center">
                   <Col>
-                    <Title level={2}>{family}</Title>
+                    <Title level={2}>{familyMap[familyId].name}</Title>
                   </Col>
                 </Row>
                 <FamilyDroppable
-                  mates={groups[family]}
-                  family={family}
+                  mates={groups[familyId] ?? []}
+                  droppableId={familyId}
                   direction="vertical"
                   removeMate={removeMate}
                 />
